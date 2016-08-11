84e0db1cd03c14cf50e2fc0943241b29e048c56e
yvesjans <yves@helpscout.com>
2016-08-11 12:12:52 +0200

    Add Pathogen and Vim plugins

--- .bin/install.sh
+++ .bin/install.sh
@@ -22,3 +22,15 @@ else
 fi;
 config checkout
 config config status.showUntrackedFiles no
+
+# Install Vim Plugins
+echo "Installing Vim plugins";
+mkdir ~/vim/bundle;
+cd ~/vim/bundle;
+git clone https://github.com/ctrlpvim/ctrlp.vim.git
+git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/scrooloose/syntastic.git
+git clone https://github.com/vim-airline/vim-airline
+git clone https://github.com/vim-airline/vim-airline-themes
+git clone https://github.com/dougireton/vim-chef.git
+git clone https://github.com/tpope/vim-fugitive.git
